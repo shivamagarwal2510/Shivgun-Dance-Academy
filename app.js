1c8db2ec7bf00d86e59a3b605e52e1789d9b4edd
Shivam Agarwal <shivgun03@gmail.com>
2022-03-01 00:00:59 +0530

    adding changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express=require('express');
 const mongoose =require('mongoose');
 const path=require('path');
 const app=express();
-const port=8000;
+const port= process.env.PORT || 8000;
 //Define mongoose schema
 main().catch(err => console.log(err));
 async function main() {
